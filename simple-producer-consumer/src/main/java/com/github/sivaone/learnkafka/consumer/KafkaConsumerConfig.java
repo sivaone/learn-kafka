25c7062a18555b92666fb4f7d02420cf45930ce5
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-09-06 12:10:17 -0500

    Add error handling to kafka listener

--- simple-producer-consumer/src/main/java/com/github/sivaone/learnkafka/consumer/KafkaConsumerConfig.java
+++ simple-producer-consumer/src/main/java/com/github/sivaone/learnkafka/consumer/KafkaConsumerConfig.java
@@ -1,10 +1,26 @@
 package com.github.sivaone.learnkafka.consumer;
 
+import com.github.sivaone.learnkafka.domain.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.retrytopic.RetryTopicConfiguration;
+import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.util.backoff.FixedBackOff;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @Slf4j
@@ -17,5 +33,55 @@ public class KafkaConsumerConfig {
                 (record, exception) -> log.error(record.key().toString(), exception),
                 new FixedBackOff(0L, 2L)
         );
+    }
+
+
+    // Non blocking Retry configuration
+//    @Bean
+    public RetryTopicConfiguration myRetryTopic(KafkaTemplate<String, Product> template) {
+        return RetryTopicConfigurationBuilder
+                .newInstance()
+//                .retryOn(IOException.class)
+                .fixedBackOff(3000)
+                .maxAttempts(4)
+                .create(template);
+    }
+
+    // Consumer factory example with DeserializationException handling
+    // @Bean
+    public ConsumerFactory<String, Product> consumerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("bootstrap.servers", "127.0.0.1:21345");
+        properties.put("group.id", "group");
+        properties.put("enable.auto.commit", false);
+        properties.put("auto.commit.interval.ms", "10");
+        properties.put("session.timeout.ms", "60000");
+
+        ErrorHandlingDeserializer<Product> errorHandlingDeserializer =
+                new ErrorHandlingDeserializer<>(new JsonDeserializer<>(Product.class));
+        return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), errorHandlingDeserializer);
+    }
+
+
+//    @Bean
+    public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+            ConsumerFactory<Object, Object> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        configurer.configure(factory, consumerFactory);
+        factory.setCommonErrorHandler(errorHandler());
+
+        return factory;
+    }
+
+//    @Bean
+    KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(ConsumerFactory<String, Product> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Product> kafkaListenerContainerFactory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        kafkaListenerContainerFactory.setConcurrency(2);
+        kafkaListenerContainerFactory.setConsumerFactory(consumerFactory);
+        kafkaListenerContainerFactory.setCommonErrorHandler(errorHandler());
+        return kafkaListenerContainerFactory;
     }
 }
--- simple-producer-consumer/src/main/java/com/github/sivaone/learnkafka/consumer/KafkaProductConsumer.java
+++ simple-producer-consumer/src/main/java/com/github/sivaone/learnkafka/consumer/KafkaProductConsumer.java
@@ -3,7 +3,16 @@ package com.github.sivaone.learnkafka.consumer;
 import com.github.sivaone.learnkafka.domain.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.converter.ConversionException;
+import org.springframework.kafka.support.serializer.DeserializationException;
+import org.springframework.messaging.converter.MessageConversionException;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.invocation.MethodArgumentResolutionException;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,8 +21,33 @@ public class KafkaProductConsumer {
 
 
     @KafkaListener(topics = {"${app.product.consumer.topic}"})
-    public void onMessage(ConsumerRecord<String, Product> productRec) {
+    // Non blocking Retry
+   /* @RetryableTopic(
+            // kafkaTemplate = "kafkaTemplate",
+            // autoCreateTopics = "false",
+            exclude = {
+                    DeserializationException.class,
+                    MessageConversionException.class,
+                    ConversionException.class,
+                    MethodArgumentResolutionException.class,
+                    NoSuchMethodException.class,
+                    ClassCastException.class
+            },
+            attempts = "4",
+            backoff = @Backoff(delay = 3000, multiplier = 1.5, maxDelay = 15000)
+    )*/
+    public void onMessage(
+            @Header(KafkaHeaders.RECEIVED_TOPIC) String receivedTopic,
+            ConsumerRecord<String, Product> productRec
+    ) {
+        log.info("Received topic: {}", receivedTopic);
         productRec.headers().forEach(h -> log.info(h.key() + " : " + new String(h.value())));
         log.info("Product update key: {}, value: {}", productRec.key(), productRec.value());
+    }
+
+
+    @DltHandler
+    public void processMessage(ConsumerRecord<String, Product> productRec) {
+        log.error("Product update key: {}, value: {}", productRec.key(), productRec.value());
     }
 }
